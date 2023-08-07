35b1e9041049f3922f2dfd54af91881c1a3cc19f
Denis Bogusiewicz <denis.bogusiewicz@gmail.com>
2023-08-07 22:56:52 +0200

    Add comments about simple operations on repository

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findAll();
+        
+        dd($movies);
 
-        $movies = ["Avengers: Endgame", "Inception", "Loki", "Black Widow"];
-
-        return $this->render('index.html.twig', array(
-            'movies' => $movies
-        ));
+        return $this->render('index.html.twig');
     }
         
     

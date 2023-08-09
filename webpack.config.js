22648a93e68fde2e3edb108297befba9a2479cf2
Denis Bogusiewicz <denis.bogusiewicz@gmail.com>
2023-08-09 14:54:18 +0200

    add file-loader to project for load images in my project

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,15 @@ Encore
 
   // uncomment if you're having problems with a jQuery plugin
   //.autoProvidejQuery()
-  .enablePostCssLoader(options);
+  .enablePostCssLoader((options) => {
+    options.postcssOptions = {
+      config: "./postcss.config.js",
+    };
+  })
+  .copyFiles({
+    from: "./assets/images",
+    to: "images/[path][name].[hash:8].[ext]",
+    pattern: /\.(png|jpg|jpeg)$/,
+  });
 
 module.exports = Encore.getWebpackConfig();

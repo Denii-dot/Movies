7ed715d264b84a04168e8ba7fd736268daf23f1b
Denis Bogusiewicz <denis.bogusiewicz@gmail.com>
2023-08-09 14:34:44 +0200

    add tailwindcss, plurgecss, postcss to my project and configurate tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -52,22 +52,23 @@ Encore
   .configureBabelPresetEnv((config) => {
     config.useBuiltIns = "usage";
     config.corejs = "3.23";
-  });
+  })
 
-// enables Sass/SCSS support
-//.enableSassLoader()
+  // enables Sass/SCSS support
+  //.enableSassLoader()
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
+  // uncomment if you use TypeScript
+  //.enableTypeScriptLoader()
 
-// uncomment if you use React
-//.enableReactPreset()
+  // uncomment if you use React
+  //.enableReactPreset()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-//.enableIntegrityHashes(Encore.isProduction())
+  // uncomment to get integrity="..." attributes on your script & link tags
+  // requires WebpackEncoreBundle 1.4 or higher
+  //.enableIntegrityHashes(Encore.isProduction())
 
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
+  // uncomment if you're having problems with a jQuery plugin
+  //.autoProvidejQuery()
+  .enablePostCssLoader(options);
 
 module.exports = Encore.getWebpackConfig();

9496e76be1ac3c08b3eb71f7c6a5e82d45da3efe
Denis Bogusiewicz <denis.bogusiewicz@gmail.com>
2023-08-09 15:50:23 +0200

    add simple views to CRUD operations on movies

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -1,24 +1,21 @@
 <?php
 
 namespace App\Controller;
-
-use App\Entity\Movie;
 use App\Repository\MovieRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $movieRepository;
 
-    private $em;
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(MovieRepository $movieRepository)
     {
-        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'movies')]
+    #[Route('/movies', methods:['GET'], name: 'movies')]
     public function index(): Response
     {
         //findAll() - SELECT * FROM movies;
@@ -28,10 +25,22 @@ class MoviesController extends AbstractController
         //count() - SELECT COUNT() FROM movies WHERE id = 1; 
         //getClassName - get name of namespace
 
-        
-        
+        $movies = $this->movieRepository->findAll();
 
-        return $this->render('index.html.twig');
+        return $this->render('movies/index.html.twig',[
+            'movies' => $movies
+        ]);
+    }
+
+    #[Route('/movies/{id}', methods:['GET'], name: 'show_movie')]
+    public function show($id): Response
+    {
+
+        $movie = $this->movieRepository->find($id);
+
+        return $this->render('movies/show.html.twig',[
+            'movie' => $movie
+        ]);
     }
         
     

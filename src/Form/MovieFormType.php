29089314144adcc9d8cd08516de74acf4b749ca6
Denis Bogusiewicz <denis.bogusiewicz@gmail.com>
2023-08-10 19:03:13 +0200

    Adding video editing capabilities to the project

--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -37,12 +37,16 @@ class MovieFormType extends AbstractType
                 ),
                 'label' => false
             ])
-            ->add('imagePath', FileType::class, [
-                'attr' => array(
-                    'class' => 'py-10'
-                ),
-                'label' => false
-            ])
+            ->add('imagePath', FileType::class, array(
+                'required' => false,
+                'mapped' => false
+            ))
+            // ->add('imagePath', FileType::class, [
+            //     'attr' => array(
+            //         'class' => 'py-10'
+            //     ),
+            //     'label' => false
+            // ])
             // ->add('actors')
         ;
     }
